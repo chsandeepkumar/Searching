bf4811a54562d30756ff42bfdc3fc9a54fd1d21b
chintalapally sandeep reddy <sandeep1212.net@gmail.com>
2019-10-22 10:08:45 -0500

    updated the sourcecode after removing console.writeLine statements

--- BinarySearching/BinarySearchService.cs
+++ BinarySearching/BinarySearchService.cs
@@ -2,21 +2,19 @@ public class BinarySearchService
 {
     public static int SearchItem(int[]inputCollection,int targetItem)
     {
-        System.Console.WriteLine("hi");
+        
         if(inputCollection.Length==0) return -1;
         int startIndex=0;
         int maxIndex=inputCollection.Length-1;
         while(startIndex<=maxIndex)
         {
-            int guessItemIndex=ComputeGuessItemIndex(startIndex,maxIndex);
-            System.Console.WriteLine(guessItemIndex);
+            int guessItemIndex=ComputeGuessItemIndex(startIndex,maxIndex);        
             if(targetItem==inputCollection[guessItemIndex]) return guessItemIndex;
 
             if(targetItem<inputCollection[guessItemIndex])
             maxIndex=guessItemIndex-1;
             else
-            startIndex=guessItemIndex+1;
-            System.Console.WriteLine("hi in loop");
+            startIndex=guessItemIndex+1;            
         }
 
         return -1;
