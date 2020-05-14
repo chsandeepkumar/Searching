e45074705b3ca5280288d1f661d3c41f065b0876
chsandeepkumar <ch.sandeepreddy544@gmail.com>
2020-05-13 21:27:26 -0500

    pritn sub array elements

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int [] inputArray={1,2,3,4};
+        ArrayUtils.PrintSubArray(inputArray);
+        Console.ReadLine();
+        }
+    }
+}
